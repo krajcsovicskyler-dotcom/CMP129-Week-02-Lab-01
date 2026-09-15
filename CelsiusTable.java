47abcbe97d81116607f8097b93b257472a319a4b
krajcsovicskyler-dotcom <krajcsovics.kyler@student.ccm.edu>
2026-09-15 16:55:00 -0400

    completed problem 1, started work on problem 2

--- CelsiusTable.java
+++ CelsiusTable.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
 public class CelsiusTable {
-    
-}
+    public static void main(String args[])
+    {
+        0; f<21; f++0)
+        double f = 35;
+        System.out.printf("%.2f", celsius(f));
+    }//ends main
+
+    public static double celsius(double f)
+    {
+        double fahrenheit = f;
+        double celsius = 5.0/9.0 * (f - 32);
+        return celsius;
+    }//ends celsius conversion method
+}//ends class
--- KineticEnergy.java
+++ KineticEnergy.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
 public class KineticEnergy {
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        double m = 0; //kilograms
+        double v = 0; //meters per second
+        System.out.println("This program will provide the amount of kinetic energy an object has rounded to the nearest hundredth.");
+        System.out.println("Please enter your objects mass in kilograms.");
+        m = input.nextDouble();
+        System.out.println("Please enter your objects velocity in meters per second.");
+        v = input.nextDouble();
+        double KE = kineticEnergy(m, v);
+        System.out.printf("Your object has a kinetic energy of %.2f joules", KE);
+    }//ends main
 
-}
+    public static double kineticEnergy(double m, double v)
+    {
+        double mass = m;
+        double velocity = v;
+        double kineticEnergy = .5 * (m * Math.pow(v, 2));
+        return kineticEnergy;
+    }//ends KE function
+}//ends class
